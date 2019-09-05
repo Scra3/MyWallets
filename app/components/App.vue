f31949e0cf7bfff1de3b99f994575da3ada4a0f5
alban bertolini <alban@sogilis.com>
2019-09-05 23:39:50 +0200

    init

---
+++ app/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <FlexboxLayout backgroundColor="#3c495e">
+            <Label text="third" class="test"/>
+        </FlexboxLayout>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hello Worlddezdezoezdezdezds!'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    Page {
+        .test {
+            color: blue;
+        }
+    }
+</style>
