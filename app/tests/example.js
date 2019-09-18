bfe438a667843bfb493c008a03531f10764a77a7
alban bertolini <alban@sogilis.com>
2019-09-18 23:37:42 +0200

    add unit testing

--- app/tests/example.js
+++ app/tests/example.js
@@ -1,6 +1,10 @@
-// A sample Jasmine test
+import Vue from 'nativescript-vue';
+import HomePage from '@/pages/HomePage';
+
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+    const Constructor = Vue.extend(HomePage);
+    const HomePagesComponent = new Constructor().$mount();
+    expect(HomePagesComponent.$el.textContent).to.contain('No Wallet added');
   });
 });
