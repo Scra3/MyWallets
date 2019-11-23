7207c4aade998d4b2bf7ba5b943819b16bac402a
alban bertolini <alban@sogilis.com>
2019-11-23 10:10:58 +0100

    [tec] ignore some files to report coverage

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   verbose: true,
-  moduleFileExtensions: ["android.js", "ios.js", "js", "json", "vue"],
-  moduleDirectories: ["node_modules"],
+  moduleFileExtensions: ['android.js', 'ios.js', 'js', 'json', 'vue'],
+  moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     // Default NativeScript webpack aliases
-    "~/(.*)$": "<rootDir>/app/$1",
-    "@/(.*)$": "<rootDir>/app/$1",
-    "^projectRoot/(.*)$": "<rootDir>/$1"
+    '~/(.*)$': '<rootDir>/app/$1',
+    '@/(.*)$': '<rootDir>/app/$1',
+    '^projectRoot/(.*)$': '<rootDir>/$1'
   },
   transform: {
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-    ".*\\.(vue)$": "vue-jest"
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
   },
   transformIgnorePatterns: [],
   collectCoverage: true,
-  collectCoverageFrom: ["app/**/*.{js,vue}"],
-  coverageReporters: ["text", "html"],
-  setupFiles: ["<rootDir>/jest/nativescript-vue-stubs.js"]
-};
+  collectCoverageFrom: ['app/**/*.{js,vue}', '!app/main.js', '!app/store.js'],
+  coverageReporters: ['text', 'html'],
+  setupFiles: ['<rootDir>/jest/nativescript-vue-stubs.js']
+}
