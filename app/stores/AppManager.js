edef41400350d4b70f0655fb32cf2f0c9f624541
alban bertolini <alban@sogilis.com>
2020-04-27 20:26:10 +0200

    [fix] add promises in manager

--- app/stores/AppManager.js
+++ app/stores/AppManager.js
@@ -27,61 +27,77 @@ export const AppManager = {
   },
   actions: {
     init(context) {
-      new Sqlite(DB_NAME).then(
-        db => {
-          db.execSQL(
-            'CREATE TABLE IF NOT EXISTS app (id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-              'currency TEXT)'
-          ).then(
-            () => {
-              context.commit('init', { database: db })
-            },
-            error => {
-              console.log('CREATE TABLE ERROR', error)
-            }
-          )
-        },
-        error => {
-          console.log('OPEN DB ERROR', error)
-        }
-      )
+      return new Promise((resolve, reject) => {
+        new Sqlite(DB_NAME).then(
+          db => {
+            db.execSQL(
+              'CREATE TABLE IF NOT EXISTS app (id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+                'currency TEXT)'
+            ).then(
+              () => {
+                context.commit('init', { database: db })
+                resolve()
+              },
+              error => {
+                console.log('CREATE TABLE ERROR', error)
+                reject()
+              }
+            )
+          },
+          error => {
+            console.log('OPEN DB ERROR', error)
+          }
+        )
+      })
     },
     insert(context, app) {
-      context.state.database
-        .execSQL('INSERT INTO app (currency) VALUES (?)', [app.currency])
-        .then(
-          id => {
-            app.id = id
-            context.commit('save', app)
-          },
-          error => {
-            console.log('INSERT ERROR', error)
-          }
-        )
+      return new Promise((resolve, reject) => {
+        context.state.database
+          .execSQL('INSERT INTO app (currency) VALUES (?)', [app.currency])
+          .then(
+            id => {
+              app.id = id
+              context.commit('save', app)
+              resolve()
+            },
+            error => {
+              console.log('INSERT ERROR', error)
+              reject()
+            }
+          )
+      })
     },
     update(context, app) {
-      context.state.database
-        .execSQL(
-          `UPDATE app SET currency = '${app.currency}' WHERE id = ${app.id}`
-        )
-        .then(
-          () => {
-            context.commit('update', app)
-          },
-          error => {
-            console.log('UPDATE ERROR', error)
-          }
-        )
+      return new Promise((resolve, reject) => {
+        context.state.database
+          .execSQL(
+            `UPDATE app SET currency = '${app.currency}' WHERE id = ${app.id}`
+          )
+          .then(
+            () => {
+              context.commit('update', app)
+              resolve()
+            },
+            error => {
+              console.log('UPDATE ERROR', error)
+              reject()
+            }
+          )
+      })
     },
     select(context) {
-      context.state.database.all('SELECT id, currency FROM app', []).then(
-        app => {
-          context.commit('load', app[0])
-        },
-        error => {
-          console.log('SELECT ERROR', error)
-        }
-      )
+      return new Promise((resolve, reject) => {
+        context.state.database.all('SELECT id, currency FROM app', []).then(
+          app => {
+            context.commit('load', app[0])
+            resolve()
+          },
+          error => {
+            console.log('SELECT ERROR', error)
+            reject()
+          }
+        )
+      })
     }
   }
 }
--- app/stores/WalletManager.js
+++ app/stores/WalletManager.js
@@ -61,101 +61,121 @@ export const WalletManager = {
   },
   actions: {
     init(context) {
-      new Sqlite(DB_NAME).then(
-        db => {
-          db.execSQL(
-            `CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, ` +
-              'investment REAL, ' +
-              'coinId TEXT NOT NULL, ' +
-              'address TEXT, ' +
-              'isUsingLocalBalance INTEGER NOT NULL, ' +
-              'balance TEXT)'
-          ).then(
-            () => {
-              context.commit('init', { database: db })
-            },
-            error => {
-              console.log('CREATE TABLE ERROR', error)
-            }
-          )
-        },
-        error => {
-          console.log('OPEN DB ERROR', error)
-        }
-      )
+      return new Promise((resolve, reject) => {
+        new Sqlite(DB_NAME).then(
+          db => {
+            db.execSQL(
+              `CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, ` +
+                'investment REAL, ' +
+                'coinId TEXT NOT NULL, ' +
+                'address TEXT, ' +
+                'isUsingLocalBalance INTEGER NOT NULL, ' +
+                'balance TEXT)'
+            ).then(
+              () => {
+                context.commit('init', { database: db })
+                resolve()
+              },
+              error => {
+                console.log('CREATE TABLE ERROR', error)
+                reject()
+              }
+            )
+          },
+          error => {
+            console.log('OPEN DB ERROR', error)
+          }
+        )
+      })
     },
     insert(context, wallet) {
-      removeUndefinedAttributeValues(wallet)
+      return new Promise((resolve, reject) => {
+        removeUndefinedAttributeValues(wallet)
 
-      context.state.database
-        .execSQL(
-          `INSERT INTO ${TABLE_NAME} (investment, coinId, address, isUsingLocalBalance, balance) VALUES (?, ?, ?, ?, ?)`,
-          [
-            wallet.investment,
-            wallet.coin.id,
-            wallet.address,
-            convertBooleanToNumericBoolean(wallet.isUsingLocalBalance),
-            wallet.balance
-          ]
-        )
-        .then(
-          id => {
-            wallet.id = id
-            context.commit('save', wallet)
-          },
-          error => {
-            console.log('INSERT ERROR', error)
-          }
-        )
+        context.state.database
+          .execSQL(
+            `INSERT INTO ${TABLE_NAME} (investment, coinId, address, isUsingLocalBalance, balance) VALUES (?, ?, ?, ?, ?)`,
+            [
+              wallet.investment,
+              wallet.coin.id,
+              wallet.address,
+              convertBooleanToNumericBoolean(wallet.isUsingLocalBalance),
+              wallet.balance
+            ]
+          )
+          .then(
+            id => {
+              wallet.id = id
+              context.commit('save', wallet)
+              resolve()
+            },
+            error => {
+              console.log('INSERT ERROR', error)
+              reject()
+            }
+          )
+      })
     },
     update(context, wallet) {
-      removeUndefinedAttributeValues(wallet)
+      return new Promise((resolve, reject) => {
+        removeUndefinedAttributeValues(wallet)
 
-      context.state.database
-        .execSQL(
-          `UPDATE ${TABLE_NAME} SET investment = ${wallet.investment}, ` +
-            `address = '${replaceNullValueToBlank(wallet.address)}', ` +
-            `isUsingLocalBalance = ${convertBooleanToNumericBoolean(
-              wallet.isUsingLocalBalance
-            )}, ` +
-            `balance = ${wallet.balance} ` +
-            `WHERE id = ${wallet.id}`
-        )
-        .then(
-          () => {
-            context.commit('update', wallet)
-          },
-          error => {
-            console.log('UPDATE ERROR', error)
-          }
-        )
+        context.state.database
+          .execSQL(
+            `UPDATE ${TABLE_NAME} SET investment = ${wallet.investment}, ` +
+              `address = '${replaceNullValueToBlank(wallet.address)}', ` +
+              `isUsingLocalBalance = ${convertBooleanToNumericBoolean(
+                wallet.isUsingLocalBalance
+              )}, ` +
+              `balance = ${wallet.balance} ` +
+              `WHERE id = ${wallet.id}`
+          )
+          .then(
+            () => {
+              context.commit('update', wallet)
+              resolve()
+            },
+            error => {
+              console.log('UPDATE ERROR', error)
+              reject()
+            }
+          )
+      })
     },
     selectAll(context) {
-      context.state.database
-        .all(
-          `SELECT id, investment, coinId, address, isUsingLocalBalance, balance FROM ${TABLE_NAME}`,
-          []
-        )
-        .then(
-          persistedWallets => {
-            context.commit('load', persistedWallets)
-          },
-          error => {
-            console.log('SELECT ERROR', error)
-          }
-        )
+      return new Promise((resolve, reject) => {
+        context.state.database
+          .all(
+            `SELECT id, investment, coinId, address, isUsingLocalBalance, balance FROM ${TABLE_NAME}`,
+            []
+          )
+          .then(
+            persistedWallets => {
+              context.commit('load', persistedWallets)
+              resolve()
+            },
+            error => {
+              console.log('SELECT ERROR', error)
+              reject()
+            }
+          )
+      })
     },
     delete(context, id) {
-      context.state.database
-        .execSQL(`DELETE FROM ${TABLE_NAME} WHERE id = ?`, [id])
-        .then(
-          () => {
-            context.commit('delete', id)
-          },
-          error => {
-            console.log('DELETE ERROR', error)
-          }
-        )
+      return new Promise((resolve, reject) => {
+        context.state.database
+          .execSQL(`DELETE FROM ${TABLE_NAME} WHERE id = ?`, [id])
+          .then(
+            () => {
+              context.commit('delete', id)
+              resolve()
+            },
+            error => {
+              console.log('DELETE ERROR', error)
+              reject()
+            }
+          )
+      })
     }
   }
 }
